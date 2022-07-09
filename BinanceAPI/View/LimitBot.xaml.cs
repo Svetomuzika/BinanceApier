d72afce809c5be56d350ae818a48409364a0c21c
Svetomuzika <sozonov_artem@mail.ru>
2022-07-09 14:43:14 +0500

    Теперь бот принимает настройки. Но их нужно допилить

--- BinanceAPI/View/LimitBot.xaml.cs
+++ BinanceAPI/View/LimitBot.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.RegularExpressions;
+﻿using BinanceAPI.Model;
+using System;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Input;
 
@@ -6,8 +8,10 @@ namespace BinanceAPI.View
 {
     public partial class LimitBot : Window
     {
-        public LimitBot()
+        private int id;
+        public LimitBot(int id)
         {
+            this.id = id;
             InitializeComponent();
         }
 
@@ -16,6 +20,27 @@ namespace BinanceAPI.View
 
             Regex regex = new Regex("[^0-9.]+");
             e.Handled = regex.IsMatch(e.Text);
+        }
+
+        public void ChangeBotSettings_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var i in BotsList.botsList)
+            {
+                if (i.Id == id)
+                {
+                    var s = SizeBot.Text.Replace('.', ',');
+                    var d = DeltaBot.Text.Replace('.', ',');
+                    var sd = SmartDeltaBot.Text.Replace('.', ',');
+                    var t = TimeBot.Text.Replace('.', ',');
+
+                    i.Size = decimal.Parse(s);
+                    i.Delta = decimal.Parse(d);
+                    i.SmartDelta = decimal.Parse(sd);
+                    i.Time = decimal.Parse(t);
+                    
+                    return;
+                }
+            }
         }
     }
 }
